e4e30879fa872cddd0c8cd34ff3011fd66a0f649
broteinheit <ai23m008@technikum-wien.at>
2023-11-29 00:48:35 +0100

    Add Window Events (log) + implement app close event

--- Viz-Engine/Viz-Engine/src/Viz/Application.cpp
+++ Viz-Engine/Viz-Engine/src/Viz/Application.cpp
@@ -1,19 +1,34 @@
 #include "vizpch.h"
 #include "Application.h"
 
-#include "Viz/Events/ApplicationEvent.h"
-
 #include <GLFW/glfw3.h>
 
 namespace Viz {
 
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e) 
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		VIZ_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 	void Application::Run()
--- Viz-Engine/Viz-Engine/src/Viz/Application.h
+++ Viz-Engine/Viz-Engine/src/Viz/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Viz/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Viz {
@@ -13,7 +14,11 @@ namespace Viz {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
